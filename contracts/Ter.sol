adfd6dbed7441d3ca50e234d760c2ce29b06353e
kinleydorji <kd77144@gmail.com>
2025-07-04 03:21:15 +0000

    removed ownable and onlyowner for mint and burn

--- contracts/Ter.sol
+++ contracts/Ter.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Ter is ERC20, Ownable{
-    constructor() ERC20("Royal Monetary Authority", "Ter") Ownable(msg.sender){
+contract Ter is ERC20{
+    constructor() ERC20("Royal Monetary Authority", "Ter"){
         _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
 
-    function mint(uint256 amount)public onlyOwner{
+    function mint(uint256 amount)public{
         _mint(msg.sender, amount);
     }
 
-    function burn(uint256 amount) public onlyOwner{
+    function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const BLOCK_SIZE = 30;
 const DROP_INTERVAL = 800;
 
 const Ter_CONTRACT_ABI = require("./contracts/Ter.json").abi;
-const Ter_CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const Ter_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const TETROMINOS = {
   0: { shape: [[0]], color: "0,0,0" },
